9b8009a3f357cab85964d08247219a3edafc869f
bbryram25 <nichakul.kon@student.mahidol.ac.th>
2025-11-03 23:31:11 +0700

    add testBoard() testBoardOneNullSquare()

---
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -0,0 +1,28 @@
+package nl.tudelft.jpacman.board;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BoardTest {
+    @Test
+    void testBoard() {
+        Square s1 = new BasicSquare();
+        Square[][] grid = new Square[1][1];
+        grid[0][0] = s1;
+        Board board = new Board(grid);
+        assertThat(board.invariant()).isTrue();
+    }
+
+    @Test
+    void testBoardOneNullSquare() {
+        Square[][] grid = new Square[1][1];
+        grid[0][0] = null;
+        try {
+            Board board = new Board(grid);
+            board.squareAt(0, 0);
+        } catch (AssertionError e) {
+            assertThat(e).hasMessageContaining("Initial grid cannot contain null squares");
+        }
+    }
+}
