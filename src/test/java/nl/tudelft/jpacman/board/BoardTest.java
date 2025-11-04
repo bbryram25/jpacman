0b06fdec04aff255c9672fe9afac54d8e66dcff3
bbryram25 <nichakul.kon@student.mahidol.ac.th>
2025-11-04 15:06:55 +0700

    add javadoc comment

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -3,7 +3,9 @@ package nl.tudelft.jpacman.board;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+/***
+ * This is a testcase for  BoardTest
+ */
 public class BoardTest {
     /***
      * This is a testcase for testBoard
